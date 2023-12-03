c4200450576f5a0814364161e831bb0307f46e47
Nipuna Harshana <nipuna.harshana@syscolabs.com>
2023-12-03 15:36:00 +0530

    change primary theme color

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './App.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
